4e39171fdfde2e43b81480066fb2cb2acc2a3804
James Garfield <jamesgarfield@gmail.com>
2024-11-13 10:23:54 -0500

    Expose brew after install

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,10 @@ if [[ -f "/usr/local/bin/brew" ]]; then
 	echo "Homebrew already installed, moving on"
 else
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
+	echo >> /Users/garf/.zprofile
+	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/garf/.zprofile
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+fi	
 
 if [[ -f "/usr/local/bin/ansible" ]]; then
 	echo "Ansible already installed"
